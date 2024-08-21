93f973127da12c83b30634908928f3403ad3cb79
Adenilson365 <adekonzelmann@gmail.com>
2024-08-21 12:02:01 -0300

    Configurando uvicorn

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+import uvicorn;
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"msg": "Hello World!"}
+    return {"msg": "Hello World!!"}
 
 @app.get("/welcome")
 async def welcome():
@@ -14,3 +15,6 @@ async def welcome():
 @app.get("/pr")
 async def pr():
     return {"msg":"Olá! Você está na minha aplicação de estudos Git e API"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info", reload=True)
